834c4958b59a80da2b26a7001c1e82afbabbc054
boooooooorbs <boooooooorbs@gmail.com>
2023-08-11 13:17:49 -0300

    Coloca link se tweet tiver mais de 300 caracteres

--- src/createProperties.ts
+++ src/createProperties.ts
@@ -16,9 +16,9 @@ export default async (item: FeedEntry) => {
   let rt = new RichText({ text: title });
   await rt.detectFacets(agent);
 
-  // Reduzir a parte da URL
+  // Encurte a parte do URL
   let text = rt.text;
-  let targets: { key: string; link: string }[] = [];
+  let targets: { link: string }[] = [];
   rt.facets?.forEach((v) => {
     if (
       v.features[0]['$type'] === 'app.bsky.richtext.facet#link' &&
@@ -30,42 +30,49 @@ export default async (item: FeedEntry) => {
           ? link
           : splitter.splitGraphemes(link).slice(0, 27).join('') + '...';
       text = text.replace(link, key);
-      targets = [...targets, { key, link }];
+      targets = [...targets, { link }];
     }
   });
 
-  // Se o texto tiver mais de 300 caracteres, segura em 300 caracteres
+  // Se tiver mais de 300 caracteres, segura em 300 caracteres
   const max = 300;
-  const isOverLength = splitter.countGraphemes(text) > max;
   const shortenedLink =
     splitter.countGraphemes(link) <= 27
       ? link
       : splitter.splitGraphemes(link).slice(0, 27).join('') + '...';
+  targets = [...targets, { link }];
+  const isOverLength =
+    splitter.countGraphemes(text) >
+    max - splitter.countGraphemes(` ${shortenedLink}`);
   if (isOverLength) {
-    const ellipsis = `...\n\n${shortenedLink}`;
-    const cnt = max - splitter.countGraphemes(ellipsis);
+    const ellipsis = `...`;
+    const cnt =
+      max - splitter.countGraphemes(`${ellipsis} ${shortenedLink}`);
     const shortenedText = splitter.splitGraphemes(text).slice(0, cnt).join('');
-    text = `${shortenedText}${ellipsis}`;
+    text = `${shortenedText}${ellipsis} ${shortenedLink}`;
   }
+  // Adicione um URL abreviado ao final do corpo
+  text = `${text}`;
 
   rt = new RichText({ text });
   await rt.detectFacets(agent);
 
-  // Substitua o link da URL encurtada pelo link original
-  rt.facets?.forEach((v, i) => {
+  // Substitua o destino do URL encurtado pelo URL original
+  const facets = rt.facets || [];
+  facets.forEach((v, i) => {
     if (
       v.features[0]['$type'] === 'app.bsky.richtext.facet#link' &&
       typeof v.features[0].uri === 'string' &&
       targets[i]?.link
     ) {
-      // Se o número de caracteres exceder o limite, o último link deve ser o link original
-      v.features[0].uri =
-        isOverLength && rt.facets && i === rt.facets.length - 1
-          ? link
-          : targets[i].link;
+      // Se o limite de caracteres for excedido, o último link deve ser o URL original
+      const uri =
+        isOverLength && i === facets.length - 1 ? link : targets[i].link;
+      v.features[0].uri = uri;
+      v.index.byteEnd = v.index.byteStart + splitter.countGraphemes(uri);
     }
   });
 
   console.log('success createProperties');
-  return { text: rt.text, facets: rt.facets, title, link };
+  return { text: rt.text, facets, title, link };
 };
